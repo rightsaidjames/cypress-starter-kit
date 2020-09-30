ab7b9b6facb95ebed13d7d8ce757dbfa4d1e6ea4
Cameron Robson <36795917+Cameronjpr@users.noreply.github.com>
2020-09-30 09:50:36 +0100

    Add viewport preset command, make default viewport size explicit (#1)
    * add viewport command, make default vport size explicit
    
    * Remove viewportWidth, viewportHeight from cypress.json
    
    Given that the values specified here are the default values for these environment variables, there's no need to specify them in cypress.json. The custom command will work as it did before.
    
    Co-authored-by: James Sheasby Thomas <Hello@rightsaidjames.com>

--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -35,3 +35,28 @@ Cypress.Commands.add('login', (user, password) => {
 
   cy.get('form#login').submit()
 })
+
+Cypress.Commands.add('viewportPreset', (size = '') => {
+  switch (size) {
+    case 'samsung-s10-plus':
+      cy.viewport(412, 869)
+      break
+    case 'iphone-se': 
+      cy.viewport(375, 667)
+      break
+    case 'ipad-pro':
+      cy.viewport(1366, 1024)
+      break
+    case 'ms-surface':
+      cy.viewport(1280, 720)
+      break
+    case 'full-hd':
+      cy.viewport(1920, 1080)
+      break
+    case 'imac':
+      cy.viewport(2560, 1440)
+      break
+    default:
+      cy.viewport(Cypress.env('viewportWidth'), Cypress.env('viewportHeight'))
+  }
+})
